62733b4bab8abb52c57161a0248718df2c883d50
mahmoudyaseen <39798017+mahmoudyaseen@users.noreply.github.com>
2018-12-22 18:56:15 +0200

    update_SignIn

--- WindowsFormsApp1/Boundary/SignInForm.cs
+++ WindowsFormsApp1/Boundary/SignInForm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//YASEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEN
+
 namespace WindowsFormsApp1.Boundary
 {
     public partial class SignInForm : Form
@@ -16,8 +16,18 @@ namespace WindowsFormsApp1.Boundary
         {
             InitializeComponent();
         }
+        Services.VerificationServices verification = new Services.VerificationServices();
         private void signinbutn_Click(object sender, EventArgs e)
         {
+            if(verification.VerifyData(usernametext.Text , passwordtext.Text))
+            {
+                Program.username = usernametext.Text;
+                this.Visible = false;
+                new PostSearchForm().ShowDialog();
+                this.Close();
+            }
+            else
+                MessageBox.Show("You entered Wrong username or Password", "Sign in", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
         private void closebutn_Click(object sender, EventArgs e)
